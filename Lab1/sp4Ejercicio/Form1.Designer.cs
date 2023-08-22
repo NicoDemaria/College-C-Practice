a680635f070241c242c98b9c7d39b468323d429d
Nicolas <nicolasdemaria4@gmail.com>
2023-08-22 12:42:39 -0300

    1

---
+++ Lab1/sp4Ejercicio/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace sp4Ejercicio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Names = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comidas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BebidaNoAlchohol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BebidaAlcohol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Postre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnValidarDatos = new System.Windows.Forms.Button();
+            this.btnMozoDia = new System.Windows.Forms.Button();
+            this.MozoDia = new System.Windows.Forms.TextBox();
+            this.btnTotales = new System.Windows.Forms.Button();
+            this.TotalDia = new System.Windows.Forms.Label();
+            this.TotalComida = new System.Windows.Forms.Label();
+            this.TotalBebidaNoAlcohol = new System.Windows.Forms.Label();
+            this.TotalBebidaAlcohol = new System.Windows.Forms.Label();
+            this.TotalPostre = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Names,
+            this.Comidas,
+            this.BebidaNoAlchohol,
+            this.BebidaAlcohol,
+            this.Postre});
+            this.dataGridView1.Location = new System.Drawing.Point(82, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(646, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Names
+            // 
+            this.Names.HeaderText = "Names";
+            this.Names.Name = "Names";
+            // 
+            // Comidas
+            // 
+            this.Comidas.HeaderText = "Comidas";
+            this.Comidas.Name = "Comidas";
+            // 
+            // BebidaNoAlchohol
+            // 
+            this.BebidaNoAlchohol.HeaderText = "BebidaNoAlchohol";
+            this.BebidaNoAlchohol.Name = "BebidaNoAlchohol";
+            // 
+            // BebidaAlcohol
+            // 
+            this.BebidaAlcohol.HeaderText = "BebidaAlcohol";
+            this.BebidaAlcohol.Name = "BebidaAlcohol";
+            // 
+            // Postre
+            // 
+            this.Postre.HeaderText = "Postre";
+            this.Postre.Name = "Postre";
+            // 
+            // btnValidarDatos
+            // 
+            this.btnValidarDatos.Location = new System.Drawing.Point(643, 149);
+            this.btnValidarDatos.Name = "btnValidarDatos";
+            this.btnValidarDatos.Size = new System.Drawing.Size(75, 23);
+            this.btnValidarDatos.TabIndex = 1;
+            this.btnValidarDatos.Text = "Validar Datos";
+            this.btnValidarDatos.UseVisualStyleBackColor = true;
+            this.btnValidarDatos.Click += new System.EventHandler(this.btnValidarDatos_Click);
+            // 
+            // btnMozoDia
+            // 
+            this.btnMozoDia.Location = new System.Drawing.Point(12, 308);
+            this.btnMozoDia.Name = "btnMozoDia";
+            this.btnMozoDia.Size = new System.Drawing.Size(80, 23);
+            this.btnMozoDia.TabIndex = 2;
+            this.btnMozoDia.Text = "Mozo del Dia";
+            this.btnMozoDia.UseVisualStyleBackColor = true;
+            // 
+            // MozoDia
+            // 
+            this.MozoDia.Location = new System.Drawing.Point(98, 308);
+            this.MozoDia.Name = "MozoDia";
+            this.MozoDia.Size = new System.Drawing.Size(100, 20);
+            this.MozoDia.TabIndex = 4;
+            this.MozoDia.Tag = "";
+            // 
+            // btnTotales
+            // 
+            this.btnTotales.Location = new System.Drawing.Point(12, 337);
+            this.btnTotales.Name = "btnTotales";
+            this.btnTotales.Size = new System.Drawing.Size(80, 23);
+            this.btnTotales.TabIndex = 5;
+            this.btnTotales.Text = "Totales";
+            this.btnTotales.UseVisualStyleBackColor = true;
+            // 
+            // TotalDia
+            // 
+            this.TotalDia.AutoSize = true;
+            this.TotalDia.Location = new System.Drawing.Point(98, 342);
+            this.TotalDia.Name = "TotalDia";
+            this.TotalDia.Size = new System.Drawing.Size(34, 13);
+            this.TotalDia.TabIndex = 6;
+            this.TotalDia.Text = "Total ";
+            this.TotalDia.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TotalComida
+            // 
+            this.TotalComida.AutoSize = true;
+            this.TotalComida.Location = new System.Drawing.Point(220, 292);
+            this.TotalComida.Name = "TotalComida";
+            this.TotalComida.Size = new System.Drawing.Size(69, 13);
+            this.TotalComida.TabIndex = 7;
+            this.TotalComida.Text = "Total Comida";
+            this.TotalComida.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TotalBebidaNoAlcohol
+            // 
+            this.TotalBebidaNoAlcohol.AutoSize = true;
+            this.TotalBebidaNoAlcohol.Location = new System.Drawing.Point(306, 292);
+            this.TotalBebidaNoAlcohol.Name = "TotalBebidaNoAlcohol";
+            this.TotalBebidaNoAlcohol.Size = new System.Drawing.Size(119, 13);
+            this.TotalBebidaNoAlcohol.TabIndex = 8;
+            this.TotalBebidaNoAlcohol.Text = "Total  BebidaNoAlcohol";
+            this.TotalBebidaNoAlcohol.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TotalBebidaAlcohol
+            // 
+            this.TotalBebidaAlcohol.AutoSize = true;
+            this.TotalBebidaAlcohol.Location = new System.Drawing.Point(421, 292);
+            this.TotalBebidaAlcohol.Name = "TotalBebidaAlcohol";
+            this.TotalBebidaAlcohol.Size = new System.Drawing.Size(105, 13);
+            this.TotalBebidaAlcohol.TabIndex = 9;
+            this.TotalBebidaAlcohol.Text = "Total  BebidaAlcohol";
+            this.TotalBebidaAlcohol.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TotalPostre
+            // 
+            this.TotalPostre.AutoSize = true;
+            this.TotalPostre.Location = new System.Drawing.Point(532, 292);
+            this.TotalPostre.Name = "TotalPostre";
+            this.TotalPostre.Size = new System.Drawing.Size(67, 13);
+            this.TotalPostre.TabIndex = 10;
+            this.TotalPostre.Text = "Total  Postre";
+            this.TotalPostre.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.TotalPostre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(699, 415);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.TotalPostre);
+            this.Controls.Add(this.TotalBebidaAlcohol);
+            this.Controls.Add(this.TotalBebidaNoAlcohol);
+            this.Controls.Add(this.TotalComida);
+            this.Controls.Add(this.TotalDia);
+            this.Controls.Add(this.btnTotales);
+            this.Controls.Add(this.MozoDia);
+            this.Controls.Add(this.btnMozoDia);
+            this.Controls.Add(this.btnValidarDatos);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Names;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comidas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BebidaNoAlchohol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BebidaAlcohol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Postre;
+        private System.Windows.Forms.Button btnValidarDatos;
+        private System.Windows.Forms.Button btnMozoDia;
+        private System.Windows.Forms.TextBox MozoDia;
+        private System.Windows.Forms.Button btnTotales;
+        private System.Windows.Forms.Label TotalDia;
+        private System.Windows.Forms.Label TotalComida;
+        private System.Windows.Forms.Label TotalBebidaNoAlcohol;
+        private System.Windows.Forms.Label TotalBebidaAlcohol;
+        private System.Windows.Forms.Label TotalPostre;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Lab1/sp4Ejercicio/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sp4Ejercicio
+{
+
+    
+    public partial class Form1 : Form
+
+
+        
+    {
+        
+        private float[,] ventas;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            ventas = new float[5, 5]; // Crear el arreglo con el tamaño adecuado
+
+            // Asignar el evento CellEndEdit al DataGridView
+                dataGridView1.CellEndEdit += dataGridView1_CellEndEdit;
+
+            // Codigo para agregar filas
+
+            void AgregarFilas()
+
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    dataGridView1.Rows.Add();
+                }
+            }
+
+            AgregarFilas();
+
+            // Supongamos que esta función se llama después de haber agregado las filas al DataGridView
+             void AgregarNombresAColumna()
+            {
+                // Lista de nombres que quieres agregar
+                List<string> nombres = new List<string>
+                            {
+                                "Julio",
+                                "Esteban",
+                                "Javier",
+                                "Gonza",
+                                "Alberto"
+                            };
+
+                // Itera a través de las filas y agrega nombres a la columna "Name"
+                for (int i = 0; i < nombres.Count && i < dataGridView1.Rows.Count; i++)
+                {
+                    dataGridView1.Rows[i].Cells["Names"].Value = nombres[i];
+                }
+            }
+                  AgregarNombresAColumna();
+
+
+
+
+
+        }
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 1 && e.ColumnIndex <= 4) // Verificar que estamos en una celda editable
+            {
+                DataGridViewCell cell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                float.TryParse(cell.Value?.ToString(), out float valor); // Intentar convertir el valor a float
+                ventas[e.RowIndex, e.ColumnIndex - 1] = valor; // Actualizar el valor en el arreglo
+                if (cell.Value is int value && value >= 0)
+                {
+                    
+                    cell.Style.BackColor = dataGridView1.DefaultCellStyle.BackColor; // Restaurar el color de fondo original
+                }
+                else
+                {
+                    cell.Style.BackColor = Color.Red; // Cambiar el color de fondo de la celda inválida
+                }
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnValidarDatos_Click(object sender, EventArgs e)
+        {
+           
+            
+
+        }
+
+    }
+}
