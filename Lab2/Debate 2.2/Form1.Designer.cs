1c2864a37fcef703a3af2008f338856bb106c594
Nicolas <nicolasdemaria4@gmail.com>
2023-08-25 14:52:56 -0300

    1

---
+++ Lab2/Debate 2.2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Debate_2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxMeses = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxImporte = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonCarga = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ColumnMeses = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnImporte = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxMeses
+            // 
+            this.comboBoxMeses.FormattingEnabled = true;
+            this.comboBoxMeses.Location = new System.Drawing.Point(401, 106);
+            this.comboBoxMeses.Name = "comboBoxMeses";
+            this.comboBoxMeses.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMeses.TabIndex = 0;
+            this.comboBoxMeses.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Meses:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Importe";
+            // 
+            // textBoxImporte
+            // 
+            this.textBoxImporte.Location = new System.Drawing.Point(401, 145);
+            this.textBoxImporte.Name = "textBoxImporte";
+            this.textBoxImporte.Size = new System.Drawing.Size(121, 20);
+            this.textBoxImporte.TabIndex = 3;
+            this.textBoxImporte.TextChanged += new System.EventHandler(this.textBoxImporte_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnMeses,
+            this.ColumnImporte});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(354, 175);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonCarga
+            // 
+            this.buttonCarga.Location = new System.Drawing.Point(541, 106);
+            this.buttonCarga.Name = "buttonCarga";
+            this.buttonCarga.Size = new System.Drawing.Size(75, 49);
+            this.buttonCarga.TabIndex = 5;
+            this.buttonCarga.Text = "Cargar y Refrescar";
+            this.buttonCarga.UseVisualStyleBackColor = true;
+            this.buttonCarga.Click += new System.EventHandler(this.buttonCarga_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(300, 195);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 200);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Listado nums";
+            // 
+            // ColumnMeses
+            // 
+            this.ColumnMeses.HeaderText = "Meses";
+            this.ColumnMeses.Name = "ColumnMeses";
+            this.ColumnMeses.ReadOnly = true;
+            // 
+            // ColumnImporte
+            // 
+            this.ColumnImporte.HeaderText = "Importe";
+            this.ColumnImporte.Name = "ColumnImporte";
+            this.ColumnImporte.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 541);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonCarga);
+            this.Controls.Add(this.textBoxImporte);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxMeses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxMeses;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxImporte;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonCarga;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMeses;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnImporte;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Lab2/Debate 2.2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Debate_2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] meses = new string[12] {
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
+        decimal[] importes = new decimal[12];
+
+        int indice = 0;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxMeses.Items.Clear();    
+            for (int i = 0; i < meses.Length; i++)
+            {
+                comboBoxMeses.Items.Add(meses[i]);
+            }
+            comboBoxMeses.SelectedIndex = 0;
+            cargarMeses();
+
+        }
+        private void cargarMeses()
+        {
+
+            for (int i = 0; i < meses.Length; i++)
+            {
+                dataGridView1.Rows.Add(meses[i]);
+            }
+        }
+
+        private void textBoxImporte_TextChanged(object sender, EventArgs e)
+        {
+
+           
+
+        }
+        
+        public void cargarMonto()
+        {
+            {
+                int i = comboBoxMeses.SelectedIndex;
+                decimal importe = Convert.ToDecimal(textBoxImporte.Text);
+                importes[i] = importe;
+
+            }
+        }
+
+        private void agregarFilasYColumnas()
+        {
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < meses.Length; i++)
+            {
+                dataGridView1.Rows.Add(meses[i], importes[i]);
+            }
+        }
+
+
+        private void buttonCarga_Click(object sender, EventArgs e)
+        {
+            cargarMonto();
+            agregarFilasYColumnas();
+        }
+    }
+}
